1be74a6e2972cf7dd531e92c9a1d906a554110d5
Wendell Oliveira <wendelloliveirasud@gmail.com>
2023-02-15 18:53:00 -0300

    navbar 50%

---
+++ src/pages/Articles.tsx
@@ -0,0 +1,11 @@
+
+
+const Article = function articles() {
+  return (
+    <div className="home">
+      <h1>Articles</h1>
+    </div>
+  );
+}
+
+export default Article;
---
+++ src/pages/Projects.tsx
@@ -0,0 +1,11 @@
+
+
+const Projects = function projects() {
+  return (
+    <div className="home">
+      <h1>Projects</h1>
+    </div>
+  );
+}
+
+export default Projects;
