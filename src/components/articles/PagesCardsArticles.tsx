164a21bfb48f02d0c741c81d18fabc250277223f
Wendell Oliveira <wendelloliveirasud@gmail.com>
2023-04-24 08:34:36 -0300

    search bar skeleton finished, put the search bar in stand-by not using yet

--- src/components/articles/PagesCardsArticles.tsx
+++ src/components/articles/PagesCardsArticles.tsx
@@ -26,7 +26,7 @@ const PagesCardsArticles = function pagesCardsArticles() {
   const [disableButton, setDisableButton] = useState(false);
 
   return (
-    <div className="flex flex-col relative w-full h-full items-center gap-6 snap-y snap-mandatory">
+    <div className="flex flex-col w-full h-full items-center gap-6">
       {articles.map((article, index) => {
         const link =
           "/articles/" +
@@ -48,29 +48,33 @@ const PagesCardsArticles = function pagesCardsArticles() {
           </NavLink>
         );
       })}
-      <div className="flex flex-row fixed bottom-8 w-52 h-12">
-        <button
-          className={`flex w-52 h-full bg-gradient-to-b from-sky-400 to-cyan-400 rounded-3xl shadow-lg justify-center items-center ${
-            disableButton && "cursor-not-allowed"
-          } hover:from-sky-500 hover:to-cyan-500`}
-          onClick={() => {
-            if (cutStart + number_of_articles >= ArticlesData.length) {
-              setDisableButton(true);
-            }
-            setArticles((current) => [
-              ...current,
-              ...ArticlesData.slice(
-                cutStart + number_of_articles,
-                cutEnd + number_of_articles
-              ),
-            ]);
-            setCutStart((current) => current + number_of_articles);
-            setcutEnd((current) => current + number_of_articles);
-          }}
-          disabled={disableButton}
-        >
-          <span className="font-[Lexend] text-white text-lg">+ Show More</span>
-        </button>
+      <div className="flex flex-col relative w-full justify-center items-center">
+        <div className="flex flex-row fixed bottom-8 w-52 h-12">
+          <button
+            className={`flex w-52 h-full bg-gradient-to-b  from-sky-400 to-cyan-400 rounded-3xl shadow-lg justify-center items-center ${
+              disableButton && "cursor-not-allowed"
+            } hover:from-sky-500 hover:to-cyan-500`}
+            onClick={() => {
+              if (cutStart + number_of_articles >= ArticlesData.length) {
+                setDisableButton(true);
+              }
+              setArticles((current) => [
+                ...current,
+                ...ArticlesData.slice(
+                  cutStart + number_of_articles,
+                  cutEnd + number_of_articles
+                ),
+              ]);
+              setCutStart((current) => current + number_of_articles);
+              setcutEnd((current) => current + number_of_articles);
+            }}
+            disabled={disableButton}
+          >
+            <span className="font-[Lexend] text-white text-lg">
+              + Show More
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/components/navbar/SearchBar.tsx
+++ src/components/navbar/SearchBar.tsx
@@ -2,32 +2,33 @@ import { useState } from "react";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 const SearchBar = function searchBar() {
-  const [inputF, setInputF] = useState(false);
+  const [inputF, setInputF] = useState(true);
 
   return (
-    <div className="nav-button-search justify-between">
+    <div className="flex h-[53%] w-[28rem] justify-between items-center text-white invisible">
       <div className="flex w-full h-full items-center">
-        <label
-          htmlFor="searchtab"
-          className="flex-none absolute search-text ml-8"
-          hidden={inputF}
-        >
-          Search (alt+s)
-        </label>
         <input
           id="searchtab"
-          className="grid relative search w-full h-full focus:outline-none focus:ring-2 focus:ring-gray-400 rounded-l"
+          className="grid relative font-[Lexend] font-light w-full h-full border-t border-l border-b border-[#f5f5f538] bg-[#ffffff3f] content-center rounded-l indent-4 focus:outline-none focus:ring-1 focus:ring-gray-400 placeholder:text-slate-200"
+          placeholder="Search"
           type="search"
           onChange={(e) => {
             console.log(e.target.value === "");
           }}
-          onFocus={(e) => {
-            setInputF(true);
-          }}
-          onBlur={(e) => {
-            e.target.value === "" ? setInputF(false) : null;
-          }}
+          onFocus={() => setInputF(false)}
+          onBlur={() => setInputF(true)}
         />
+        <div
+          className={`flex flex-col absolute top-[3.6rem] w-[25.9rem] h-[25rem] bg-white rounded-lg ${
+            inputF ? "collapse" : ""
+          }`}
+        >
+          <div className="flex flex-row w-full basis-1/5 bg-gray-200 rounded-t-lg"></div>
+          <div className="flex flex-row w-full basis-1/5 bg-sky-200"></div>
+          <div className="flex flex-row w-full basis-1/5 bg-cyan-200"></div>
+          <div className="flex flex-row w-full basis-1/5 bg-pink-200"></div>
+          <div className="flex flex-row w-full basis-1/5 bg-orange-200 rounded-b-lg"></div>
+        </div>
       </div>
       <button className="grid w-9 h-full place-content-center search-btn focus:outline-none focus:ring-2 focus:ring-gray-400 rounded-r">
         <MagnifyingGlassIcon className="h-6 w-6 stroke-white pointer-events-none" />
