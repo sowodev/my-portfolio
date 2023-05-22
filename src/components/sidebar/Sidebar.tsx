02b00b8813d9a37ccb23ebb1b89abb68b48a00cb
Wendell Oliveira <wendelloliveirasud@gmail.com>
2023-05-22 08:41:34 -0300

    fixed some minor issues with the sidebar

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,9 @@
 import {
   HomeIcon,
+  MoonIcon,
   NewspaperIcon,
   RocketLaunchIcon,
+  SunIcon,
   UserCircleIcon,
 } from "@heroicons/react/24/outline";
 import React, {
@@ -11,14 +13,13 @@ import React, {
   useEffect,
   useRef,
 } from "react";
-import DarkModeBtn from "../global-btns/DarkModeBtn";
 import { Transition } from "@headlessui/react";
 import { GlobalContext } from "../commom/context/GlobalContext";
 import { NavLink } from "react-router-dom";
 
 const Sidebar: React.FC = () => {
   const sidebar_ref: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null);
-  const { set_sidebar } = useContext(GlobalContext);
+  const { set_dark, set_sidebar } = useContext(GlobalContext);
 
   const handleOutsideClick = (e: MouseEvent) => {
     if (
@@ -27,6 +28,16 @@ const Sidebar: React.FC = () => {
     ) {
       set_sidebar.setOpenSidebar(false);
     }
+  };
+
+  const handleThemeChange = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
+
+    set_dark.setIsDark(set_dark.is_dark === "light" ? "dark" : "light");
+    localStorage.setItem(
+      "theme",
+      set_dark.is_dark === "light" ? "dark" : "light"
+    );
   };
 
   // useEffect for handling outside click
@@ -56,35 +67,90 @@ const Sidebar: React.FC = () => {
             <div className="flex flex-col w-full h-fit justify-start items-center gap-4">
               <NavLink
                 to={"/"}
-                className="flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 hover:bg-[#606060]"
+                className={`flex w-full h-fit justify-center items-center`}
+                onClick={() => set_sidebar.setOpenSidebar(false)}
               >
-                <HomeIcon className={`w-6 h-6 stroke-white`} />
-                <span className="font-[Blinker] text-white">Home</span>
+                {({ isActive }) => {
+                  return (
+                    <div
+                      className={`flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 ${
+                        isActive && `bg-neutral-500 border-l-[6px] border-white`
+                      } hover:bg-[#606060]`}
+                    >
+                      <HomeIcon className={`w-6 h-6 stroke-white`} />
+                      <span className="font-[Blinker] text-white">Home</span>
+                    </div>
+                  );
+                }}
               </NavLink>
               <NavLink
                 to={"/articles"}
-                className="flex flex-row w-4/5 h-10 bg-neutral-500 border-l-[6px] border-white rounded justify-start items-center ps-8 gap-4"
+                className={`flex w-full h-fit justify-center items-center`}
+                onClick={() => set_sidebar.setOpenSidebar(false)}
               >
-                <NewspaperIcon className={`w-6 h-6 stroke-white`} />
-                <span className="font-[Blinker] text-white">Blog</span>
+                {({ isActive }) => {
+                  return (
+                    <div
+                      className={`flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 ${
+                        isActive && `bg-neutral-500 border-l-[6px] border-white`
+                      } hover:bg-[#606060]`}
+                    >
+                      <NewspaperIcon className={`w-6 h-6 stroke-white`} />
+                      <span className="font-[Blinker] text-white">Blog</span>
+                    </div>
+                  );
+                }}
               </NavLink>
               <NavLink
                 to={"/projects"}
-                className="flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 hover:bg-[#606060]"
+                className={`flex w-full h-fit justify-center items-center`}
+                onClick={() => set_sidebar.setOpenSidebar(false)}
               >
-                <RocketLaunchIcon className={`w-6 h-6 stroke-white`} />
-                <span className="font-[Blinker] text-white">Projects</span>
+                {({ isActive }) => {
+                  return (
+                    <div
+                      className={`flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 ${
+                        isActive && `bg-neutral-500 border-l-[6px] border-white`
+                      } hover:bg-[#606060]`}
+                    >
+                      <RocketLaunchIcon className={`w-6 h-6 stroke-white`} />
+                      <span className="font-[Blinker] text-white">
+                        Projects
+                      </span>
+                    </div>
+                  );
+                }}
               </NavLink>
               <NavLink
                 to={"/login"}
-                className="flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 hover:bg-[#606060]"
+                className={`flex w-full h-fit justify-center items-center`}
+                onClick={() => set_sidebar.setOpenSidebar(false)}
               >
-                <UserCircleIcon className={`w-6 h-6 stroke-white`} />
-                <span className="font-[Blinker] text-white">Sing In</span>
+                {({ isActive }) => {
+                  return (
+                    <div
+                      className={`flex flex-row w-4/5 h-10 rounded justify-start items-center ps-8 gap-4 ${
+                        isActive && `bg-neutral-500 border-l-[6px] border-white`
+                      } hover:bg-[#606060]`}
+                    >
+                      <UserCircleIcon className={`w-6 h-6 stroke-white`} />
+                      <span className="font-[Blinker] text-white">Sing In</span>
+                    </div>
+                  );
+                }}
               </NavLink>
             </div>
             <div className="w-fit h-fit ps-8">
-              <DarkModeBtn />
+              <button
+                className="flex w-9 h-9 rounded shadow-md border border-neutral-500 justify-center items-center hover:bg-neutral-500 hover:scale-110 dark:border-gray-700 dark:bg-slate-800 dark:shadow-slate-600 dark:hover:bg-slate-700 transition ease-in-out duration-300"
+                onClick={handleThemeChange}
+              >
+                {set_dark.is_dark === "dark" ? (
+                  <SunIcon className="w-5 h-5 stroke-slate-200" />
+                ) : (
+                  <MoonIcon className="w-5 h-5 stroke-white" />
+                )}
+              </button>
             </div>
           </div>
         </div>
