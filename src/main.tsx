bd4b796430800f881e7be03748fb50204f866877
Wendell Oliveira <wendelloliveirasud@gmail.com>
2023-05-24 19:39:36 -0300

    added react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const query_client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={query_client}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
